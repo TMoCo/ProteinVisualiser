cc95b38abbb874a8f0099df45bd1b27edaeadc4e
Tommy <sc17tm@leeds.ac.uk>
2020-05-03 12:37:16 +0100

    removed curves and splines tutorial, added an executable for project

--- Curves and Splines tutorial/Assets/Line.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Line : MonoBehaviour
-{
-    // line going from p0 to p1
-    public Vector3 p0 = new Vector3(0f, 0f, 0f), p1 = new Vector3(0f, 0f, 0f);
-}
